fa2476e947c5f52e7d4ae2a4dad059f848963895
RAJ <rajmishra.ydd@gmail.com>
2026-04-11 15:02:23 +0530

    Fixed puppeteer launch flags for EC2

--- src/utils/pdfGenerator.js
+++ src/utils/pdfGenerator.js
@@ -2,7 +2,10 @@ const puppeteer = require("puppeteer");
 
 const generatePdf = async (htmlContent) => {
     try {
-        const browser = await puppeteer.launch();
+        const browser = await puppeteer.launch({
+            headless: 'new', // Use the latest headless mode
+            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+        });
         const page = await browser.newPage();
         await page.setContent(htmlContent);
         const pdfBuffer = await page.pdf();
