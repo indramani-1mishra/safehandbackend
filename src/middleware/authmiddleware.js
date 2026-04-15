9ca98fc02839c96f861e6700cb47ce4ec5ab56fd
RAJ <rajmishra.ydd@gmail.com>
2026-04-15 16:52:08 +0530

    code updated

--- src/middleware/authmiddleware.js
+++ src/middleware/authmiddleware.js
@@ -18,6 +18,7 @@ const authMiddleware = async (req, res, next) => {
 const isAdmin = (req, res, next) => {
     try {
         if (!req.user) {
+
             return res.status(401).json({ message: "User not authenticated" });
         }
         if (req.user.role !== "admin") {
@@ -45,10 +46,22 @@ const workerAuthMiddleware = async (req, res, next) => {
 }
 
 const allowAnyAuth = (req, res, next) => {
-    console.log(req.worker, req.admin);
-    if (req.worker || req.admin) {
+    const token = req.cookies.adminToken;
+    const workerToken = req.cookies.workerAccessToken;
+    console.log("token", token);
+    console.log("workerToken", workerToken);
+    if (token) {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        req.admin = decoded;
         return next();
     }
+    if (workerToken) {
+        const decoded = jwt.verify(workerToken, JWT_SECRET);
+        req.worker = decoded;
+
+        return next();
+    }
+
     return res.status(403).json({ message: "Unauthorized" });
 };
 
