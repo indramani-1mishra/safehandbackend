e8bb8ea5d478add8afcdc21bfc40f64ca78ec763
indramani mishra <rajmishrasky@gmail.com>
2026-05-16 03:43:52 -0700

    fix the backend code

--- src/server.js
+++ src/server.js
@@ -36,7 +36,6 @@ const corsOptions = {
 
 // Middlewares
 app.use(cors(corsOptions));
-app.options('/*', cors(corsOptions));
 app.use(express.json());
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ limit: "50mb", extended: true }))
